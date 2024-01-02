0ea83ccde1e7df0bea3f3c2452fa128a8fc2be74
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2024-01-02 20:03:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ import "./services/axiosHelper";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
