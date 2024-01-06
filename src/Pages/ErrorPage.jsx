6a38da41b8a47504666057c6118db5cd1dbfc8ff
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2024-01-06 12:17:21 +0200

    Update ErrorPage.jsx

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,32 +1,30 @@
-import {Container, Grid, Typography, Button} from '@mui/material';
-import PageHeader from '../Layout/header/PageHeader';
-
+import { Container, Grid, Typography, Button } from "@mui/material";
+import PageHeader from "../Layout/header/PageHeader";
+import ROUTES from "../routes/ROUTES";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return(
-        <Container sx={{backgroundColor:'azure'}}>
-            <PageHeader title='Error 404' subtitle='Page Not Found'/>
+  return (
+    <Container>
+      <PageHeader title="Error 404" subtitle="Page Not Found" />
 
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={8}>
-                    <Typography variant="h5" color="initial">
-                        Ooops... The Request URL wan not found on this server
-                    </Typography>
-                    <Button variant="text" color="primary">
-                        CLICK HERE TO RETURN TO THE hOME PAGE...
-                    </Button> 
-                </Grid>
-                <Grid item xs={12} md={4} justifyContent='center' alignItems='center'>
-                    <img
-                        width='60%'
-                        src='/assets/imgs/404.jpg'
-                        alt='404'
-                    />
-                </Grid>
-            </Grid>
-        </Container>
-
-    );
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Typography variant="h5" color="initial">
+            Ooops... The Request URL wan not found on this server
+          </Typography>
+          <Link to={ROUTES.HOME}>
+            <Button variant="text" color="primary">
+              CLICK HERE TO RETURN TO THE hOME PAGE...
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={4} justifyContent="center" alignItems="center">
+          <img width="60%" src="/assets/imgs/404.jpg" alt="404" />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default ErrorPage;
