a6c6f4b241ba9523b1703fef5eae0042ff7114d2
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2024-01-27 21:17:14 +0200

    changing instead of calling the link children, i'd rather call it tilte to prevent confusion

--- src/Layout/header/ui/Links.jsx
+++ src/Layout/header/ui/Links.jsx
@@ -17,33 +17,33 @@ const Links = () => {
     <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
       {alwaysLinks.map((myItem, index) => (
         <NavLinkComponent to={myItem.to} key={"linksnav" + index}>
-          {myItem.children}
+          {myItem.title}
         </NavLinkComponent>
       ))}
       {loggedIn &&
         loggedInLinks.map((myItem, index) => (
           <NavLinkComponent to={myItem.to} key={"linksnav2" + index}>
-            {myItem.children}
+            {myItem.title}
           </NavLinkComponent>
         ))}
       {loggedIn &&
         loggedIn.isBusiness &&
         bizLinks.map((myItem, index) => (
           <NavLinkComponent to={myItem.to} key={"linksnav3" + index}>
-            {myItem.children}
+            {myItem.title}
           </NavLinkComponent>
         ))}
       {loggedIn &&
         loggedIn.isAdmin &&
         adminLinks.map((myItem, index) => (
           <NavLinkComponent to={myItem.to} key={"linksnav4" + index}>
-            {myItem.children}
+            {myItem.title}
           </NavLinkComponent>
         ))}
       {!loggedIn &&
         loggedOutLinks.map((myItem, index) => (
           <NavLinkComponent to={myItem.to} key={"linksnav5" + index}>
-            {myItem.children}
+            {myItem.title}
           </NavLinkComponent>
         ))}
     </Box>
--- src/Layout/myLinks.js
+++ src/Layout/myLinks.js
@@ -1,16 +1,16 @@
 import ROUTES from "../routes/ROUTES";
 
-const alwaysLinks = [{ to: ROUTES.ABOUT, children: "About Us" }];
+const alwaysLinks = [{ to: ROUTES.ABOUT, title: "About Us" }];
 
-const loggedInLinks = [{ to: ROUTES.FAV, children: "FavCards 🌟" }];
+const loggedInLinks = [{ to: ROUTES.FAV, title: "FavCards 🌟" }];
 
-const bizLinks = [{ to: ROUTES.MYCARDS, children: "MYCARDS" }];
+const bizLinks = [{ to: ROUTES.MYCARDS, title: "MYCARDS" }];
 
-const adminLinks = [{ to: ROUTES.SANDBOX, children: "UserManage" }];
+const adminLinks = [{ to: ROUTES.SANDBOX, title: "UserManage" }];
 
 const loggedOutLinks = [
-  { to: ROUTES.REGISTER, children: "Register page" },
-  { to: ROUTES.LOGIN, children: "Login page" },
+  { to: ROUTES.REGISTER, title: "Register page" },
+  { to: ROUTES.LOGIN, title: "Login page" },
 ];
 
 export { alwaysLinks, loggedInLinks, loggedOutLinks, bizLinks, adminLinks };
