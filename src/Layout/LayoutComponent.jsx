6851f692d84b459303b8849dd356cdb09f509255
YasirAssi <144779590+YasirAssi@users.noreply.github.com>
2024-02-01 20:25:31 +0200

    handling comments

--- src/Layout/LayoutComponent.jsx
+++ src/Layout/LayoutComponent.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
+import { useState } from "react";
 import FooterComponent from "./footer/FooterComponent";
 import HeaderComponent from "./header/HeaderComponent";
 import MainComponent from "./main/MainComponent";
@@ -9,39 +7,10 @@ import tmc from "twin-moon-color";
 import { CssBaseline, Typography } from "@mui/material";
 import useAutoLogin from "../hooks/useAutoLogIn";
 import PropTypes from "prop-types";
-import LogInContext from "../store/loginContext";
 
 const LayoutComponent = ({ children }) => {
   const finishAutoLogin = useAutoLogin();
   const [isDarkTheme, SetDarkTheme] = useState(false);
-  // const [userInfo, setUserInfo] = useState(null);
-  // const { logIn } = useContext(LogInContext);
-
-  // useEffect(() => {
-  //   const fetchUserInfo = async () => {
-  //     if (logIn && logIn._id) {
-  //       try {
-  //         const { data } = await axios.get("/users/" + logIn._id);
-  //         setUserInfo(data);
-  //         console.log("data", data);
-  //       } catch (error) {
-  //         console.log("err", error);
-  //         toast.error("Ops! something went wrong", {
-  //           position: "top-right",
-  //           autoClose: 3000,
-  //           hideProgressBar: false,
-  //           closeOnClick: true,
-  //           pauseOnHover: true,
-  //           draggable: true,
-  //           progress: undefined,
-  //           theme: "dark",
-  //         });
-  //       }
-  //     }
-  //   };
-
-  //   fetchUserInfo();
-  // }, [logIn, setUserInfo]);
 
   const themes = tmc({
     "text.headerColor": "!gray",
